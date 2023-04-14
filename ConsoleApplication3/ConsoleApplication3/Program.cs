694b50118b433aed6c180a8792eadc6ae8b6d9ab
samanvilly <s.ghaderian@live.fr>
2023-04-14 18:47:38 +0430

    new

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static int BMM(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return BMM(b, a % b);
+            }    
+        }
+        static int KMM(int a, int b)
+        {
+            return (a / BMM(a, b)) * b;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter firs digit");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second digit");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int kmm = KMM(num1, num2);
+            int bmm = BMM(num1, num2);
+            Console.WriteLine("KMM is: {0} " , kmm);
+            Console.WriteLine("BMM is: {0} " , bmm);
+            Console.ReadLine();
+        }
+
+    }
+}
