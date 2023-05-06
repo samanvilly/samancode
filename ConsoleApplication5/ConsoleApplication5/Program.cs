57f843df5146601d4f73c5d53d5d3039456825a1
samanvilly <s.ghaderian@live.fr>
2023-05-06 23:01:39 +0430

    project 6

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] main_arr = new int[10];
+            for (int j = 0; j < main_arr.Length; j++)
+            {
+                main_arr[j] = random.Next(1, 10);
+            }
+           
+            foreach(int temp in main_arr)
+            {
+                Console.Write(temp + " ");
+            }
+            Console.WriteLine();
+            double[] result = myfunc(main_arr);
+            foreach (double temp1 in result)
+            {
+                Console.Write(temp1.ToString("0.00") + " ");
+            }
+            Console.ReadLine();
+        }
+           
+        private static double[] myfunc(int[] func_arr)
+        {
+            double[] result = new double[10];
+
+            for (int i = 0; i < func_arr.Length; i++)
+            {
+                if (i == 0 || i== 9)
+                {
+                    result[i] = Convert.ToDouble(func_arr[i]);
+
+                }
+                else
+                {
+                    result[i] = Convert.ToDouble((func_arr[i - 1] + func_arr[i] + func_arr[i + 1]) / 3);
+                }
+            }
+            return result;
+
+          }
+                       
+    }
+}
---
+++ ConsoleApplication6/ConsoleApplication6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] matris = new int[10, 10];
+            for (int i = 0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    matris[i, j] = random.Next(1, 10);
+                }
+            }
+            for (int i=0; i < matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < matris.GetLength(1); j++)
+                {
+                    Console.Write(matris[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("*******************");
+            myfunc(matris);
+            Console.ReadLine();
+        }
+        private static void myfunc (int[,] func_matris)
+        {
+            for (int i = 1; i < func_matris.GetLength(0) - 1;i+=3){
+                for (int j = 1; j < func_matris.GetLength(1) - 1; j += 3)
+                {
+                    int sum = 0;
+                    for (int ii = i - 1; ii <= i + 1; ii++)
+                    {
+                        for (int jj = j - 1; jj <= j + 1; jj++)
+                        {
+                            if (ii == i && jj == j)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                sum += func_matris[ii, jj];
+                            }
+                        }
+                    }
+                    int miane = sum / 8;// برای محاسبه میانه 9 عتصر اگر درست حساب کرده باشم جمع 8 عنصر کناری تقسیم بر 8 محسابه میشد و جای عنصر وسطی قرار میگرفت
+                    func_matris[i, j] = miane;
+                }
+            }
+            for (int i = 0; i < func_matris.GetLength(0); i++)
+            {
+                for (int j = 0; j < func_matris.GetLength(1); j++)
+                {
+                    Console.Write(func_matris[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
