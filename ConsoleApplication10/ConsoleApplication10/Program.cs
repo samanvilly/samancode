85edc8ff04add981efa2c8c226b9a3afd60513dc
samanvilly <s.ghaderian@live.fr>
2023-05-21 10:16:50 +0330

    new commit

---
+++ ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class circle
+    {
+        private double r;
+        double pi = 3.14;
+        public double R
+        {
+            get { return r; }
+            set { if (value > 0)
+                {
+                    r = value;
+                }
+            }
+        }
+        public circle() { }
+        public circle(double R)
+        {
+            r = R;
+        }
+        public double S()
+        {
+            return Math.Pow(r, 2) * pi;
+        }
+        public double A()
+        {
+            return 2 * r * pi;
+        }
+    }
+    class Test { 
+        static void Main(string[] args)
+        {
+            double r = Convert.ToDouble(Console.ReadLine());
+            circle c1 = new circle(r);
+            double s1 = c1.S();
+            Console.WriteLine(s1);
+            double a1 = c1.A();
+            Console.WriteLine(a1);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    public class Program
+    {
+        private string id;
+        private string name;
+        private string lastname;
+        private int hours;
+        private int payperonhoure;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public int Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        public int Payperhour
+        {
+            get { return payperonhoure; }
+            set
+            {
+                if (value > 0)
+                {
+                    payperonhoure = value;
+                }
+            }
+        }
+        public long Teacher() { }
+        public long Teacher(string id, string name, string lastname, int hours, int payperhour) {
+            id = Id;
+            name = Name;
+            lastname = Lastname;
+            hours = Hours;
+            payperhour = Payperhour;
+        }
+        public long payment()
+        {
+            return hours * payperonhoure;
+        }
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
